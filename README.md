a94d87987352df5db481a3cfd9b6d4830ccb5731
mike <mikhail.papkov@gmail.com>
2017-11-09 13:15:37 +0200

    Add obligatory username validation before any new game

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # multisudoku
 Multiplayer concurrent sudoku
 
+University of Tartu. Distributed systems course. Fall 2017
+
+* Mikhail Papkov
+
+* Elizaveta Korotkova
+
+* Vladislav Fedyukov
+
+* Alina Vorontseva
+
+## Insructions
+
 `sudo apt-get install python-tk`
+
+`pip install -r requirements`
--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import Tkinter as tk
 import tkMessageBox
 import logging
 from functools import partial
+import re
 
 from sudoku import *
 
@@ -13,9 +14,6 @@ LOG = logging.getLogger()
 class SudokuFrame(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-
-        # Set validator for boxes
-        vcmd = (self.register(self.validate_entry), '%P')
 
         # Create trackers for sudoku boxes
         self.box_values = [tk.StringVar() for i in range(81)]
@@ -29,7 +27,7 @@ class SudokuFrame(tk.Frame):
                                justify='center',
                                textvariable=sv,
                                validate='key',
-                               validatecommand=vcmd,
+                               validatecommand=(self.register(self.validate_entry), '%P'),
                                state='readonly') for sv in self.box_values]
 
         # Fill the grid
@@ -54,8 +52,17 @@ class SudokuFrame(tk.Frame):
         LOG.debug("Get current state: %s" % cs)
         return cs
 
-    def generate_sudoku(self):
+    def generate_sudoku(self, valid_username=False):
         LOG.debug("Trying to generate new sudoku")
+        # If some information is not valid
+        if not valid_username:
+            LOG.debug("Generation is not allowed")
+            tkMessageBox.showinfo("Info", "Username should not be longer than "
+                                          "8 alphanumeric characters "
+                                          "(empty strings not allowed, "
+                                          "spaces not allowed)")
+            return
+
         sudoku = get_sudoku()
         unsolved = sum(sudoku["u"], [])
         LOG.debug("Got unsolved sudoku with %d elements" % len(unsolved))
@@ -71,48 +78,107 @@ class SudokuFrame(tk.Frame):
 
         # tkMessageBox.showinfo("Info", "New sudoku was generated")
 
-# Root frame
+
+# Menu frame for buttons
+class MenuFrame(tk.Frame):
+    def __init__(self, parent, frm_sudoku, width=25):
+        tk.Frame.__init__(self, parent)
+        self.title = tk.Label(self, text="Multiplayer sudoku\nv.0.0.1", font=100)
+        self.title.pack(side=tk.TOP)
+
+        # Create username frame with label and textfield
+        self.frm_username = tk.Frame(self)
+        self.frm_username.pack(side=tk.BOTTOM)
+
+        # Label
+        self.lbl_username = tk.Label(self.frm_username,
+                                     text="username")
+        self.lbl_username.pack(side=tk.LEFT)
+
+        # Test entry
+        self.sv_username = tk.StringVar()
+        self.ent_username = tk.Entry(self.frm_username,
+                                     bd=3,
+                                     textvariable=self.sv_username,
+                                     validate='key',
+                                     validatecommand=(self.register(self.validate_username), '%P'))
+        self.ent_username.pack(side=tk.RIGHT)
+
+        # Create buttons
+        self.valid_username = False
+        self.frm_sudoku = frm_sudoku
+        self.vcmd_username = partial(SudokuFrame.generate_sudoku, self.frm_sudoku, self.valid_username)
+        self.btn_new = tk.Button(self,
+                                 text="New game",
+                                 width=width,
+                                 command=self.vcmd_username)
+        self.btn_new.pack(side=tk.BOTTOM)
+
+        self.btn_connect = tk.Button(self,
+                                     width=width,
+                                     text="Connect")
+        self.btn_connect.pack(side=tk.BOTTOM)
+
+    def validate_username(self, username):
+        p = re.compile('^[0-9A-Za-z]{1,8}$')
+        if not re.match(p, username):
+            self.valid_username = False
+            self.vcmd_username = partial(SudokuFrame.generate_sudoku, self.frm_sudoku, self.valid_username)
+            self.btn_new.config(command=self.vcmd_username)
+            LOG.debug("Invalid username [%s], status %s" % (username, self.valid_username))
+        else:
+            self.valid_username = True
+            self.vcmd_username = partial(SudokuFrame.generate_sudoku, self.frm_sudoku, self.valid_username)
+            self.btn_new.config(command=self.vcmd_username)
+            LOG.debug("Valid username [%s], status %s" % (username, self.valid_username))
+        return True
+
+
+# Create frames
 root = tk.Tk()
 root.resizable(width=False, height=False)
 
 # Main pane
-main_pane = tk.PanedWindow(root, showhandle=False)
-main_pane.pack(fill=tk.BOTH, expand=True)
-
-# Menu frame for buttons
-menu_frame = tk.Frame()
-main_pane.add(menu_frame)
-title = tk.Label(menu_frame, text="Multiplayer sudoku\nv.0.0.1", font=100)
-title.pack(side=tk.TOP)
+pn_main = tk.PanedWindow(root, showhandle=False)
+pn_main.pack(fill=tk.BOTH, expand=True)
 
 # Sudoku pane
-sudoku_pane = tk.PanedWindow(main_pane, relief=tk.RAISED)
-sudoku_pane.pack(fill=tk.NONE)
-main_pane.add(sudoku_pane)
+pn_sudoku = tk.PanedWindow(pn_main, relief=tk.RAISED)
+pn_sudoku.pack(fill=tk.NONE)
+frm_sudoku = SudokuFrame(pn_sudoku)
+frm_sudoku.pack(side=tk.TOP)
 
-frame_sudoku = SudokuFrame(sudoku_pane)
-frame_sudoku.pack(side=tk.TOP)
+# Create menu frame and bound it with sudoku
+frm_menu = MenuFrame(pn_main, frm_sudoku)
+
+# Add panes to main pane
+pn_main.add(frm_menu)
+pn_main.add(pn_sudoku)
+# title = tk.Label(menu_frame, text="Multiplayer sudoku\nv.0.0.1", font=100)
+# title.pack(side=tk.TOP)
+
+
 
 # Create username
-frm_uname = tk.Frame(menu_frame)
-frm_uname.pack(side=tk.BOTTOM)
-lbl_uname = tk.Label(frm_uname, text="User Name")
-lbl_uname.pack(side=tk.LEFT)
-ent_uname = tk.Entry(frm_uname, bd=5)
-ent_uname.pack(side=tk.RIGHT)
-
-# Create menu buttons
-btn_width = 30
-btn_new_sudoku = tk.Button(menu_frame,
-                           text="New sudoku",
-                           width=btn_width,
-                           command=partial(SudokuFrame.generate_sudoku, frame_sudoku))
-btn_new_sudoku.pack(side=tk.BOTTOM)
-
-btn_connect = tk.Button(menu_frame,
-                        width=btn_width,
-                        text="Connect")
-btn_connect.pack(side=tk.BOTTOM)
+# frm_uname = tk.Frame(menu_frame)
+# frm_uname.pack(side=tk.BOTTOM)
+# lbl_uname = tk.Label(frm_uname, text="User Name")
+# lbl_uname.pack(side=tk.LEFT)
+# ent_uname = tk.Entry(frm_uname, bd=5)
+# ent_uname.pack(side=tk.RIGHT)
+#
+# # Create menu buttons
+# btn_width = 30
+# btn_new_sudoku = tk.Button(menu_frame,
+#                            text="New sudoku",
+#                            width=btn_width,
+#                            command=partial(SudokuFrame.generate_sudoku, frame_sudoku))
+# btn_new_sudoku.pack(side=tk.BOTTOM)
+#
+# btn_connect = tk.Button(menu_frame,
+#                         width=btn_width,
+#                         text="Connect")
+# btn_connect.pack(side=tk.BOTTOM)
 
 
 
